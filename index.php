8dd79fb15e947546cc1587e29d9b9442604f998b
Arjon Jason Castro <ajcastro29@gmail.com>
2024-10-01 15:45:32 +0800

    Fix typo

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ foreach (NewsManager::getInstance()->listNews() as $news) {
 	echo($news->getBody() . "\n");
 
 	// Instead of using listComments() method, we use listCommentsByNewsId() to query only the comments of a specific newsId.
-	// This has better performance because we are only selecting the specific comments related to newId.
+	// This has better performance because we are only selecting the specific comments related to newsId.
 	/** @var Comment $comment */
 	foreach (CommentManager::getInstance()->listCommentsByNewsId($news->getId()) as $comment) {
 		echo("Comment " . $comment->getId() . " : " . $comment->getBody() . "\n");
