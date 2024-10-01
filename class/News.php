cbcdcf2f3538559f12974ec498b77a9a5372faa7
Arjon Jason Castro <ajcastro29@gmail.com>
2024-10-01 15:44:27 +0800

    Fix newline

--- class/News.php
+++ class/News.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 class News
 {
 	protected int $id;
--- utils/CommentManager.php
+++ utils/CommentManager.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 class CommentManager
 {
 	private static ?self $instance = null;
